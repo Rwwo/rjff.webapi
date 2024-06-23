57285075a5d23f735ff82d19e86c123bbbc8aa7c
Rubens Facco <rubensfacco94@gmail.com>
2024-06-23 10:21:47 -0300

    Adição da controller signatário

---
+++ src/rjff.avmb.api/Controllers/SignatarioController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using rjff.avmb.application.Commands;
+using rjff.avmb.core.InputModels;
+using rjff.avmb.core.Interfaces;
+using rjff.avmb.core.Models;
+using rjff.avmb.infrastructure.Services.AstenModels;
+
+namespace rjff.avmb.api.Controllers
+{
+    [Route("api/signatario")]
+    public class SignatarioController : MainController
+    {
+        private readonly IMediator _mediator;
+        public SignatarioController(IMediator mediator, INotificador notificador) : base(notificador)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("configurar-signatario")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<GenericResult<ResponseConfigurarSignatario>>> Adicionar(ConfigurarSignatarioInputModel input)
+        {
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
+
+            var inputCommand = new ConfigurarSignatarioCommand(input);
+            var result = await _mediator.Send(inputCommand);
+
+            return CustomResponse(HttpStatusCode.OK, result);
+        }
+    }
+}
