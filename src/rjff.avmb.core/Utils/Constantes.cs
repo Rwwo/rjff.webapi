50fa2abbdd03ae64334b94963b3bf907476d8afe
Rubens Facco <rubensfacco94@gmail.com>
2024-06-23 10:25:03 -0300

    Criação/atualização de constantes mais viewmodel de retorno da criação de envelope.

--- src/rjff.avmb.core/Utils/Constantes.cs
+++ src/rjff.avmb.core/Utils/Constantes.cs
@@ -5,10 +5,10 @@
         public const string URL_BASE_HOMOLOGACAO = "https://plataforma.astenassinatura.com.br/api/";
         public const string URL_BASE_PRODUCAO = "";
 
-        public const string URL_CRIAR_ENVELOPE = "inserirenvelope";
-        public const string URL_INSERIR_SIGNATARIO_ENVELOPE = "inserirsignatarioenvelope";
-        public const string URL_ENCAMINHAR_ENVELOPE_ASSINATURA = "encaminharenvelopeparaassinaturas";
-        public const string URL_STATUS_ENVELOPE = "getvolumeenvelopesporstatus";
+        public const string URL_CRIAR_ENVELOPE = "inserirEnvelope";
+        public const string URL_INSERIR_SIGNATARIO_ENVELOPE = "inserirSignatarioEnvelope";
+        public const string URL_ENCAMINHAR_ENVELOPE_ASSINATURA = "encaminharEnvelopeParaAssinaturas";
+        public const string URL_STATUS_ENVELOPE = "getDadosEnvelope";
 
     }
 }
---
+++ src/rjff.avmb.core/ViewModel/Responses.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rjff.avmb.core.ViewModel
+{
+
+    public class StatusEnvelopeViewModel
+    {
+        public ResponseStatusEnvViewModel response { get; set; }
+    }
+
+    public class ResponseStatusEnvViewModel
+    {
+        public string id { get; set; }
+        public RepositorioViewModel Repositorio { get; set; }
+        public UsuarioViewModel Usuario { get; set; }
+        public string descricao { get; set; }
+        public string conteudo { get; set; }
+        public string incluirHashTodasPaginas { get; set; }
+        public string permitirDespachos { get; set; }
+        public string usarOrdem { get; set; }
+        public string hashSHA256 { get; set; }
+        public string hashSHA512 { get; set; }
+        public string mensagem { get; set; }
+        public string mensagemObservadores { get; set; }
+        public string motivoCancelamento { get; set; }
+        public string numeroPaginas { get; set; }
+        public string status { get; set; }
+        public DateTime dataHoraCriacao { get; set; }
+        public DateTime dataHoraAlteracao { get; set; }
+        public string objetoContrato { get; set; }
+        public string statusContrato { get; set; }
+        public string numContrato { get; set; }
+        public string descricaoContratante { get; set; }
+        public string descricaoContratado { get; set; }
+        public string bloquearDesenhoPaginas { get; set; }
+        public string Envelope { get; set; }
+    }
+
+    public class RepositorioViewModel
+    {
+        public string id { get; set; }
+    }
+
+    public class UsuarioViewModel
+    {
+        public string id { get; set; }
+    }
+
+}
