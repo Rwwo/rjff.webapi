5e04f85b6f242488cd44aad6fbcb6669a65f6783
Rubens Facco <rubensfacco94@gmail.com>
2024-06-23 10:20:30 -0300

    Ajustes de configuração do automapper para status envelope

--- src/rjff.abmv.application/Profiles/AutoMapperConfig.cs
+++ src/rjff.abmv.application/Profiles/AutoMapperConfig.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 
 using rjff.avmb.core.InputModels;
+using rjff.avmb.core.Models;
+using rjff.avmb.core.ViewModel;
 using rjff.avmb.infrastructure.Services.AstenModels;
 
 
@@ -11,6 +13,27 @@ namespace rjff.avmb.application.Profiles
     {
         public AutoMapperConfig()
         {
+            CreateMap<ConfigurarSignatarioInputModel, core.Models.ConfigurarSignatario>()
+                .ForMember(dest => dest.@params, opt => opt.MapFrom(src => src.@params));
+
+
+            CreateMap<ResponseStatusEnvelope, StatusEnvelopeViewModel>()
+                .ForMember(dest => dest.response, opt => opt.MapFrom(src => src.response));
+
+            // Mapping for Response to ResponseViewModel
+            CreateMap<ResponseStatusEnv, ResponseStatusEnvViewModel>()
+                .ForMember(dest => dest.Repositorio, opt => opt.MapFrom(src => src.Repositorio))
+                .ForMember(dest => dest.Usuario, opt => opt.MapFrom(src => src.Usuario));
+
+            // Mapping for Repositorio to RepositorioViewModel
+            CreateMap<Repositorio, RepositorioViewModel>()
+                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.id));
+
+            // Mapping for Usuario to UsuarioViewModel
+            CreateMap<Usuario, UsuarioViewModel>()
+                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.id));
+
+
             CreateMap<CriarEnvelopeInputModel, core.Models.CriarEnvelope>()
                 .ForMember(dest => dest.token, opt => opt.MapFrom(src => src.token))
                 .ForMember(dest => dest.@params, opt => opt.MapFrom(src => src.@params));
